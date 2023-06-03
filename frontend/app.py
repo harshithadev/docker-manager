27a18545ce43309126407f53ec08c39f1415f2b2
harshithadev <harshithadevineni16@gmail.com>
2023-06-03 14:23:47 +0530

    added dummy streamlit page

---
+++ frontend/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Dummy Streamlit Application")
+    st.write("Welcome to my dummy application!")
+
+    # Add your Streamlit components and logic here
+
+if __name__ == '__main__':
+    main()
